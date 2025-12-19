44b42669626150bdee276bec6ec589bf87e93318
Zhangbo Cheng <davidcheng@zhangbos-mbp.mynetworksettings.com>
2025-12-18 20:17:48 -0500

    set up uv

--- bol_agent_gui.py
+++ bol_agent_gui.py
@@ -1,3 +1,11 @@
+# /// script
+# requires-python = ">=3.10"
+# dependencies = [
+#     "selenium",
+#     "webdriver-manager",
+# ]
+# ///
+
 import tkinter as tk
 from tkinter import scrolledtext, messagebox
 import threading
